fedb6a178e07e1f66e52a0526a020c95ed93f618
RC_Student_lab <RC_Student_lab@10.0.0.136>
2024-10-30 20:19:24 +0000

    task test

---
+++ src/test/java/com/mycompany/poe_part1/TaskTest.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.poe_part1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class TaskTest {
+    
+    public TaskTest() {
+    }
+    
+    /**
+     * Test of checkTaskDescription method, of class Task.
+     */
+    @Test
+    public void testCheckTaskDescription() {
+        System.out.println("checkTaskDescription");
+        Task instance = null;
+        boolean expResult = false;
+        boolean result = instance.checkTaskDescription();
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of createTaskID method, of class Task.
+     */
+    @Test
+    public void testCreateTaskID() {
+        System.out.println("createTaskID");
+        Task instance = null;
+        String expResult = "";
+        String result = instance.createTaskID();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of printTaskDetails method, of class Task.
+     */
+    @Test
+    public void testPrintTaskDetails() {
+        System.out.println("printTaskDetails");
+        Task instance = null;
+        String expResult = "";
+        String result = instance.printTaskDetails();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of returnTotalHours method, of class Task.
+     */
+    @Test
+    public void testReturnTotalHours() {
+        System.out.println("returnTotalHours");
+        int expResult = 0;
+        int result = Task.returnTotalHours();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of addTasks method, of class Task.
+     */
+    @Test
+    public void testAddTasks() {
+        System.out.println("addTasks");
+        Task.addTasks();
+    }
+    
+}
